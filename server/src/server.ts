489cdc4f31b352185ad629836aa1f2cbe1830ea6
C1322591 Tiago Frioli Leonel <c1322591@interno.bb.com.br>
2023-01-27 14:43:22 -0300

    first commit

---
+++ server/src/server.ts
@@ -0,0 +1,15 @@
+import cors from "@fastify/cors";
+import Fastify from "fastify";
+import { AppRoutes } from "./routes";
+
+const app = Fastify();
+
+app.register(cors);
+app.register(AppRoutes);
+app
+  .listen({
+    port: 3333,
+  })
+  .then(() => {
+    console.log("Server is running on port 3333");
+  });
