891bbed06d86cb77c60ca4894a293f4c8061c445
C1322591 Tiago Frioli Leonel <c1322591@interno.bb.com.br>
2023-01-31 17:14:18 -0300

    ajustes

--- mobile-expo/src/screens/Home.tsx
+++ mobile-expo/src/screens/Home.tsx
@@ -1,6 +1,6 @@
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import { Text, View, ScrollView, Alert } from "react-native";
-import { useNavigation } from "@react-navigation/native";
+import { useFocusEffect, useNavigation } from "@react-navigation/native";
 
 import { api } from "../lib/axios";
 
@@ -41,9 +41,11 @@ export function Home() {
     }
   }
 
-  useEffect(() => {
-    fetchData();
-  }, []);
+  useFocusEffect(
+    useCallback(() => {
+      fetchData();
+    }, [])
+  );
 
   if (loading) {
     return <Loading />;
--- server/src/server.ts
+++ server/src/server.ts
@@ -9,6 +9,7 @@ app.register(AppRoutes);
 app
   .listen({
     port: 3333,
+    host: "0.0.0.0",
   })
   .then(() => {
     console.log("Server is running on port 3333");
